844479e8ce226c56d8cf8fbbbac431256a0dd641
awad-safwat2 <awad.safwat78@gmail.com>
2024-02-09 18:32:29 +0200

    start coding

---
+++ lib/music_player/music_player.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MusicPlayer extends StatelessWidget {
+  const MusicPlayer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
